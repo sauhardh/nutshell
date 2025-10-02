e7ba414d7f3acd736449ef5bb9393dac4e4ae307
ostrich-egg <sauhardhakafle12@gmail.com>
2025-10-02 23:27:29 +0545

    Feat: working Deploy server

--- deploy_server/core/__main__.py
+++ deploy_server/core/__main__.py
@@ -1,52 +1,60 @@
-from flask import Flask
 from dotenv import load_dotenv
 import os
 import logging
 from pathlib import Path
-import sys
+import time
 
 from core.redis import NutshellRedis
 from core.cloud import CloudStorage
 from core.builder import Build
 from core.utils import upload_to_bucket
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] <%(name)s>: %(message)s",
 )
 logger = logging.getLogger(__name__)
 
+
+def main(
+    redis_client: NutshellRedis, cloud: CloudStorage, _BUCKET_NAME: str, _PREFIX: str
+) -> bool:
+    # get job id from redis. job id is a unique prefix in cloud bucket, where specific files from github repo are stored.
+    next_repo = redis_client.get_current_job("jobQueue")
+    redis_client.put_status("status", key=next_repo, value="downloading...")
+
+    if not next_repo:
+        logger.info("No new jobs. Did not get any another job to continue.")
+        return False
+
+    # download objects from bucket that starts with `next_repo`
+    cloud.download_objects(_BUCKET_NAME, f"{_PREFIX}/{next_repo}/")
+    working_dir = cloud.get_working_dir()  # local path to a dir
+
+    redis_client.put_status("status", key=next_repo, value="download completed")
+
+    # to compile those downloaded objects(code) inside Docker container.
+    builder = Build().build(app_path=working_dir)
+    # local path to a compiled code, inside downloaded code.
+    build_path = builder.get_build_path()
+    base_dir = Path(build_path).parent.parent
+
+    upload_to_bucket("nutshell-build", build_path, cloud, base_dir)
+    redis_client.put_status("status", key=next_repo, value="Build completed")
+
+
 load_dotenv(dotenv_path="../.env")
-app = Flask(__name__)
 
 if __name__ == "__main__":
+    # connect to redis client
+    redis_client = NutshellRedis("localhost", 6379, 0)
+    # get bucket name and prefix(if present) from dotenv file
     _BUCKET_NAME = os.getenv("BUCKET_NAME") or "nutshell"
     _PREFIX = os.getenv("CONVENTIONAL_PREFIX") or "repos"
 
-    redis_client = NutshellRedis("localhost", 6379, 0)
-    next_repo = redis_client.get_current_job("jobQueue")
-
-    if not next_repo:
-        next_repo = "jaDlMY"
-        # continue
-
-    # cloud = CloudStorage().download_objects(_BUCKET_NAME, f"{_PREFIX}/{next_repo}/")
-    # working_dir = cloud.get_working_dir()
-
-    working_dir = f"/home/sauhardha-kafle/Desktop/nutshell/deploy_server/downloads/{_PREFIX}/{next_repo}/"
-
-    # builder = Build().build(app_path=working_dir)
-    # build_path = builder.get_build_path()
-    build_path = None
-    if not build_path:
-        logger.warning(
-            "Failed to get build_path. build_path should be retrived from `Build` object."
-        )
-        build_path = f"/home/sauhardha-kafle/Desktop/nutshell/deploy_server/downloads/{_PREFIX}/{next_repo}/build"
-
-    base_dir = Path(build_path).parent.parent
-
     cloud = CloudStorage()
-    upload_to_bucket("nutshell-build", build_path, cloud, base_dir)
-    print(next_repo)
+
+    while True:
+        if not main(redis_client, cloud, _BUCKET_NAME, _PREFIX):
+            logger.info("No jobs in the queue. Resuming normal flow after some time")
+            time.sleep(10)
--- deploy_server/core/redis.py
+++ deploy_server/core/redis.py
@@ -5,7 +5,12 @@ class NutshellRedis:
     r: Redis
 
     def __init__(self, host: str = "localhost", port: int = 6379, db=0):
-        self.r = Redis(host, port, db, protocol=3)
+        self.r = Redis(host, port, db, decode_responses=True)
 
-    def get_current_job(self, key: str = "jobQueue") -> str:
-        return self.r.lpop(key)
+    def get_current_job(self, key: str = "jobQueue") -> str | None:
+        return self.r.rpop(key)
+
+    def put_status(self, name: str, key: str, value: str = "processing"):
+        if not key:
+            key = "status"
+        return self.r.hset(name, key, value)
