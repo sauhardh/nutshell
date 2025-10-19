d66522fe9045be6a1894a0c5307ea93498d5457d
ostrich-egg <sauhardhakafle12@gmail.com>
2025-10-19 21:57:29 +0545

    scope user:email now allows to read user email

--- frontend/src/lib/authOptions.ts
+++ frontend/src/lib/authOptions.ts
@@ -7,7 +7,7 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
             authorization: {
-                params: { scope: "read:user repo" }
+                params: { scope: "read:user user:email repo" }
             }
         }),
     ],
